bb15aa9a4efec4d7b28d384ed737558af3a99659
amaglovany <artem.maglovany@gmail.com>
2018-09-18 10:15:53 +0300

    Nearly finished passengersMovement through animations

--- Assets/Scripts/PassengersGenerator.cs
+++ Assets/Scripts/PassengersGenerator.cs
@@ -11,24 +11,52 @@ public class PassengersGenerator : MonoBehaviour
     [SerializeField] private GameObject itemsPassengerPrefab;
     [SerializeField] private GameObject ultimatePassengerPrefab;
 
-    [SerializeField] private int maxPassengersOnScreenCount = 10;
-    [SerializeField] private List<GameObject> passengersOnScreen;
-    [SerializeField] private List<Vector3> passengersSpawnPointsPositions;
-    private Vector3 firstPassengerPosition;
-
     private void Start()
     {
         GeneratePassengersSpawnTransforms();
 
         GeneratePassengersQueue(defaultPassengerPrefab);
+        //MoveToPlayer();
     }
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Other condition
+        {
+            //MoveToPlayer();
+            foreach (var passenger in passengersOnScreen)
+            {
+                passenger.GetComponent<Animator>().Play("PassengerMoveAnimation");
+            }
+        }
+    }
+
+    private void MoveToPlayer()
+    {
+        foreach (var passenger in passengersOnScreen)
+        {
+            //passenger.transform.Translate(new Vector3(0f, 0f, 2.5f));
+            passenger.transform.position = Vector3.SmoothDamp(passenger.transform.position,
+                new Vector3(passenger.transform.position.x, passenger.transform.position.y,
+                    passenger.transform.position.z + 2.5f), ref velocity, .3f);
+        }
+    }
+
+    #region Generating and Spawning
+
+    [SerializeField] private int maxPassengersOnScreenCount = 10;
+    [SerializeField] private List<GameObject> passengersOnScreen;
+    [SerializeField] private List<Vector3> passengersSpawnPointsPositions;
+    private Vector3 firstPassengerPosition;
 
     private void GeneratePassengersQueue(GameObject passengerGO)
     {
         for (int i = 0; i < maxPassengersOnScreenCount; i++)
         {
             var addedPassenger = Instantiate(passengerGO, gameObject.transform);
-            
+
             addedPassenger.transform.position = passengersSpawnPointsPositions[i];
             passengersOnScreen.Add(addedPassenger);
         }
@@ -49,4 +77,6 @@ public class PassengersGenerator : MonoBehaviour
                 passengersSpawnPointsPositions[last].y, passengersSpawnPointsPositions[last].z - 2));
         }
     }
+
+    #endregion
 }
