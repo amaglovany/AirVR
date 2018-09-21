debc3978c45a92330f15c9217d92c8841f823666
amaglovany <artem.maglovany@gmail.com>
2018-09-21 18:57:05 +0300

    Update 3.5
    -Basket taking and moving done, needs some polish

--- Assets/Scripts/PlayerControlHandlerTwo.cs
+++ Assets/Scripts/PlayerControlHandlerTwo.cs
@@ -4,7 +4,51 @@ using UnityEngine;
 
 public class PlayerControlHandlerTwo : MonoBehaviour
 {
+    private RaycastHit hit;
+    private Transform pickedObject;
+    private float distance;
+    private Vector3 newPosition;
+    private Ray ray;
+
+    private bool taken;
+
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            taken = !taken;
+        }
+
+        if (taken)
+        {
+            ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0.5F));
+
+            if (!pickedObject)
+            {
+                if (Physics.Raycast(ray, out hit) && hit.transform.CompareTag("Pickable"))
+                {
+                    if (hit.rigidbody)
+                    {
+                        hit.rigidbody.velocity = Vector3.zero;
+                    }
+
+                    pickedObject = hit.transform;
+                    pickedObject.GetComponent<Rigidbody>().useGravity = false;
+                    distance = Vector3.Distance(pickedObject.position, Camera.main.transform.position);
+                }
+            }
+            else
+            {
+                newPosition = ray.GetPoint(distance);
+                pickedObject.GetComponent<Rigidbody>().MovePosition(newPosition);
+            }
+        }
+        else
+        {
+            if (pickedObject != null)
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+
+            pickedObject = null;
+        }
     }
 }
